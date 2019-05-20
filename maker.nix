b6cc00ac6080d0df0b7d26fcc1b6feaa358797c9
Christopher Fredén <me@icetan.org>
2019-05-20 15:05:12 +0200

    Add dapp2nix

--- maker.nix
+++ maker.nix
@@ -12,5 +12,10 @@ self: super: with super;
     rev = "ef77915e2881011603491275f36b44bf2478b408";
   }) { inherit pkgs; }).sethret;
 
+  dapp2nix = import (fetchGit {
+    url = https://github.com/icetan/dapp2nix.git;
+    rev = "bff040c2bc8d4cb862c47bb3d6f6e74a9c9d83b2";
+  }) { inherit pkgs; };
+
   makerScriptPackage = self.callPackage ./script-builder.nix {};
 }
